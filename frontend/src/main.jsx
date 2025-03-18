1be8f762a99aa7a9cc8af64ff13aea0727d3e3a9
tostako <juan.arteaga.f@uniautonoma.edu.co>
2025-03-18 15:31:23 -0500

    rutas nuevas en frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/podcast" element={<Podcast />} />
       <Route path="/appmovil" element={<Appmovil />} />
       <Route path="/investigaciones" element={<Investigaciones />} />
+      <Route path="/subirInvestigaciones" element={<h1>investigaciones</h1>} />
+      <Route path="/subirproyectos" element={<h1>proyectos</h1>} />
+      <Route path="/subirpodcast" element={<h1>podcast</h1>} />
+      <Route path="/subirsimulaciones" element={<h1>simulaciones</h1>} />
+
+
+
     </Routes>
   </BrowserRouter>
 );
